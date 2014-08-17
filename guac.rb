b2338b43747ecad5b632ef6206e2fae99bc33718
Christopher Bunting <christophersbunting@gmail.com>
2014-08-16 22:45:01 -0700

    Party on

--- guac.rb
+++ guac.rb
@@ -5,14 +5,17 @@ require 'dm-postgres-adapter'
 require 'dm-timestamps'
 require 'dm-core'
 require 'pg'
+require 'pry'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/database.db")
 class Partiers
   include DataMapper::Resource
   property :id, Serial
-  property :name, Text
-  property :email, Text
-  property :item, Enum[:guac, :beer, :other], default: new
+  property :name, String
+  property :email, String
+  property :guac, Boolean, default: false
+  property :beer, Boolean, default: false
+  property :other, Boolean, default: false
   property :created_at, DateTime
   property :updated_at, DateTime
 end
@@ -29,10 +32,17 @@ end
 
 post '/' do
   p = Partiers.new
-  p.name = "name"
+  p.name = params[:name]
   p.email = params[:email]
-  p.item = params[:item]
-
+  if params[:guac]
+    p.guac = true
+  end
+  if params[:beer]
+  p.beer = true
+  end
+  if params[:other]
+    p.other = true
+  end
   p.created_at = Time.now
   p.updated_at = Time.now
   if p.save
@@ -40,7 +50,7 @@ post '/' do
   else
     redirect '/', :error => 'Damn'
   end
-  # redirect '/', :notice => 'Welcome to the guacamole life.'
+  redirect '/', :notice => 'Welcome to the guacamole life.'
 end
 
 get '/success' do
